ecae940a7ac4033f8cc3dedf6517906981ff5a14
Spencer Echon <sechon01@seattlecentral.edu>
2016-03-06 21:25:31 -0800

    Commit for web client

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownListArtist" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownListArtist_SelectedIndexChanged"></asp:DropDownList>
+        <asp:DropDownList ID="DropDownListVenue" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownListVenue_SelectedIndexChanged"></asp:DropDownList>
+        <asp:GridView ID="GridView" runat="server"></asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerServiceReference.ShowTrackerServicesClient db 
+        = new ShowTrackerServiceReference.ShowTrackerServicesClient();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadDropDownArtist();
+            LoadDropDownVenue();
+        }
+    }
+
+    protected void DropDownListArtist_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FillGridArtist();
+    }
+
+    protected void DropDownListVenue_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FillGridVenue();
+    }
+
+    protected void LoadDropDownArtist()
+    {
+        string[] artists = db.GetArtists();
+        DropDownListArtist.DataSource = artists;
+        DropDownListArtist.DataBind();
+        DropDownListArtist.Items.Insert(0, "Pick an Artist");
+    }
+
+    protected void LoadDropDownVenue()
+    {
+        string[] venues = db.GetVenues();
+        DropDownListVenue.DataSource = venues;
+        DropDownListVenue.DataBind();
+        DropDownListVenue.Items.Insert(0, "Pick a Venue");
+    }
+
+    protected void FillGridArtist()
+    {
+        string artist = DropDownListArtist.SelectedItem.Text;
+        ShowTrackerServiceReference.ShowLite[] shows = db.GetShowByArtist(artist);
+        GridView.DataSource = shows;
+        GridView.DataBind();
+    }
+
+    protected void FillGridVenue()
+    {
+        string venue = DropDownListVenue.SelectedItem.Text;
+        ShowTrackerServiceReference.ShowLite[] shows = db.GetShowByVenue(venue);
+        GridView.DataSource = shows;
+        GridView.DataBind();
+    }
+}
